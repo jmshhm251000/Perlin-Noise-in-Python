e0d50b42bc5c87c264124bfaa7216467cd016b8d
RadioHead <qhdxbqm123@gmail.com>
2024-06-21 23:55:35 +1000

    added vector calculations and dot product methods

--- main.py
+++ main.py
@@ -2,46 +2,71 @@ import pygame
 import random
 import math
 
-class Vector:
+class Noise:
     def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
+
+
     def __add__(self, other):
         if isinstance(other, self.__class__):
-            return Vector(self.x + other.x, self.y + other.y)
+            return Noise(self.x + other.x, self.y + other.y)
         else:
-            return Vector(self.x + other, self.y + other)
-    def gradient(self):
+            return Noise(self.x + other, self.y + other)
+        
+
+    def gradient(self, gridpoints):
+        gridpointsgradient = []
         theta = random.uniform(0, 2 * math.pi)
-        gx = math.cos(theta)
-        gy = math.sin(theta)
-        return Vector(gx, gy)
+        for point in gridpoints:
+            gridpointsgradient.append(Noise(math.cos(theta), math.sin(theta)))
 
-grid = Vector(380, 280)
-print(grid)
-print(grid.gradient())
-
-grid = pygame.Rect(380, 280, 40, 40)
-
-
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
-pygame.init()
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-
-run = True
-while run:
-
-    pygame.draw.rect(screen, (0, 0, 255, 255), grid)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+        return gridpointsgradient
     
-    pygame.display.update()
 
-pygame.quit()
+    def gridvectorstopoints(self, gridpoints):
+        gridvectorstopoint = []
+        for point in gridpoints:
+            gridvectorstopoint.append(Noise(self.x - point.x, self.y - point.y))
+
+        return gridvectorstopoint
+    
+    
+    def dotproduct(self, gridpoints):
+        gradient = self.gradient(gridpoints)
+        gridvectors = self.gridvectorstopoints(gridpoints)
+        points = []
+        for i, point in enumerate(gridvectors):
+            point.append(points.append(Noise(gradient[i].x * point.x, gradient[i].y * point.y)))
+        
+        return points
+
+def generate_noise(width: int, height: int, scale: int, octave: int):
+    grid = []
+    for x in range(width):
+        for y in range(range):
+            grid.append(0) #will finish this method after lerping and smoothing has been implemented.
+
+def run(width, height, scale, octave):
+    octave = octave
+    SCREEN_WIDTH = width
+    SCREEN_HEIGHT = height
+    scale = scale
+
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    run = True
+    while run:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        
+        pygame.display.update()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    run(800, 800, 20, 2)
