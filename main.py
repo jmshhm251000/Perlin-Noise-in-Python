7e221012a8009e511968c7df568ad6bf006abb4a
RadioHead <qhdxbqm123@gmail.com>
2024-06-22 10:30:31 +1000

    idk what i'm doing

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ import math
 
 class Noise:
     def __init__(self, x: float, y: float):
-        self.x = x
-        self.y = y
+        self.x0 = int(x)
+        self.y0 = int(y)
+        self.x1 = x + 1
+        self.y1 = y + 1
+
+        self.sx = x - float(self.x)
+        self.sy = y - float(self.y)
 
 
     def __add__(self, other):
@@ -15,37 +20,32 @@ class Noise:
             return Noise(self.x + other, self.y + other)
         
 
-    def gradient(self, gridpoints):
-        gridpointsgradient = []
+    def gradient(self):
         theta = random.uniform(0, 2 * math.pi)
-        for point in gridpoints:
-            gridpointsgradient.append(Noise(math.cos(theta), math.sin(theta)))
 
-        return gridpointsgradient
+        return (math.cos(theta) * self.x0, math.sin(theta) * self.y0, math.cos(theta) * self.x1, math.sin(theta) * self.y1)
     
 
-    def gridvectorstopoints(self, gridpoints):
-        gridvectorstopoint = []
-        for point in gridpoints:
-            gridvectorstopoint.append(Noise(self.x - point.x, self.y - point.y))
+    def gridvectorstopoints(self, x: float, y: float):
+        return ((x - self.x0, y - self.y0), (x - self.x1, y - self.y1), (x - self.x0, y - self.y1), (x - self.x1, y - self.y0))
+    
+    
+    def dotproduct(self, x: float, y: float):
+        gx0, gy0, gx1, gy1 = self.gradient()
+        gv = self.gridvectorstopoints(x, y)
+        return (gx0 * gv[0][0] + gy0 * gv[0][1])
+    
 
-        return gridvectorstopoint
-    
-    
-    def dotproduct(self, gridpoints):
-        gradient = self.gradient(gridpoints)
-        gridvectors = self.gridvectorstopoints(gridpoints)
-        points = []
-        for i, point in enumerate(gridvectors):
-            point.append(points.append(Noise(gradient[i].x * point.x, gradient[i].y * point.y)))
-        
-        return points
+    def interpolate(self, point1, point2, t):
+        return (point2-point1) * (3.0 - t * 2.0) * t * t + point1
+
 
 def generate_noise(width: int, height: int, scale: int, octave: int):
     grid = []
-    for x in range(width):
-        for y in range(range):
-            grid.append(0) #will finish this method after lerping and smoothing has been implemented.
+    for x in range(0,width,scale):
+        for y in range(range, scale):
+            grid.append(Noise(x+random.random()*scale, y+random.randdom()*scale))
+    
 
 def run(width, height, scale, octave):
     octave = octave
